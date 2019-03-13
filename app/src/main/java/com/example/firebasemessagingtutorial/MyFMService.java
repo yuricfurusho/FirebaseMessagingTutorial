e22265da191dba9959315fbfdc0331aa7358bf24
yurifurusho <yellowocre2019>
2019-03-13 16:21:15 -0300

    force disabling notification

--- app/src/main/java/com/example/firebasemessagingtutorial/MyFMService.java
+++ app/src/main/java/com/example/firebasemessagingtutorial/MyFMService.java
@@ -30,35 +30,39 @@ public class MyFMService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // ...
+        boolean notificationEnabled = false;
+        if (notificationEnabled){
+            // ...
 
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
+            // TODO(developer): Handle FCM messages here.
+            // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+            Log.d(TAG, "From: " + remoteMessage.getFrom());
 
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            // Check if message contains a data payload.
+            if (remoteMessage.getData().size() > 0) {
+                Log.d(TAG, "Message data payload: " + remoteMessage.getData());
 
 //            if (/* Check if data needs to be processed by long running job */ true) {
-            // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
 //                scheduleJob();
 //            } else {
-            // Handle message within 10 seconds
-            handleNow();
+                // Handle message within 10 seconds
+                handleNow();
 //            }
 
+            }
+
+            // Check if message contains a notification payload.
+            if (remoteMessage.getNotification() != null) {
+                String messageBody = remoteMessage.getNotification().getBody();
+                Log.d(TAG, "Message Notification Body: " + messageBody);
+                sendNotification(messageBody);
+            }
+
+            // Also if you intend on generating your own notifications as a result of a received FCM
+            // message, here is where that should be initiated. See sendNotification method below.
         }
 
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            String messageBody = remoteMessage.getNotification().getBody();
-            Log.d(TAG, "Message Notification Body: " + messageBody);
-            sendNotification(messageBody);
-        }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
 
     }
 
